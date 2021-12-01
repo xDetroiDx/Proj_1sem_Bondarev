1718e587b6608db2878d21ac61ed333647c776ac
Dmitry <92572893+xDetroiDx@users.noreply.github.com>
2021-12-01 23:46:50 +0300

    Add files via upload

---
+++ Proj_1sem_Bondarev/PZ_5/PZ_5_2.py
@@ -0,0 +1,26 @@
+# Описать функцию DigitCountSum(K, C, S), находящую количество C цифр целого
+# положительного числа K, а также их сумму S (K — входной, C и S — выходные параметры
+# целого типа). С помощью этой функции найти количество и сумму цифр для каждого из
+# пяти данных целых чисел.
+
+import random
+
+
+def digitcountsum(k, result):
+    s = str(k)
+    n = len(s)
+    _sum = 0
+    for i in range(n):
+        _sum += int(s[i])
+    result['C'] = n
+    result['S'] = _sum
+
+
+r = {'C': None, 'S': None}
+for i in range(5):
+    K = random.randrange(1, 10000)
+    print("Число ", i + 1, ": ", K)
+    digitcountsum(K, r)
+    print('Количество цифр = ', r['C'])
+    print('Сумма цифр = ', r['S'])
+    print()
