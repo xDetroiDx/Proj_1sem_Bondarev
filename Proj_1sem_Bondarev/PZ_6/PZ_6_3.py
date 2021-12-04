7a4e1199b4fa088d280f30b47e143744c4806a7d
Dmitry <92572893+xDetroiDx@users.noreply.github.com>
2021-12-04 22:51:38 +0300

    PZ_6

---
+++ Proj_1sem_Bondarev/PZ_6/PZ_6_3.py
@@ -0,0 +1,17 @@
+# Дан список размера N. Обнулить все его локальные максимумы (то есть числа,
+# большие своих соседей).
+
+import random
+
+N = int(input("Введите размер списка: "))
+
+A = [random.randint(0, 9) for i in range(N)]
+
+print(A)
+
+for i in range(1, N-1):
+    if A[i-1] < A[i] and A[i] > A[i+1]:
+        A[i] = 9999999
+for i in range(1, N-1):
+    if A[i] == 9999999:
+        A[i] = 0
