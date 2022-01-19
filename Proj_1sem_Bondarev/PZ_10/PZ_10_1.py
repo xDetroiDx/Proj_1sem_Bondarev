7491c3e4c5140ca55955935e66141b7f490318ee
Dmitry <92572893+xDetroiDx@users.noreply.github.com>
2022-01-19 11:47:18 +0300

    Update PZ_10_1.py

--- Proj_1sem_Bondarev/PZ_10/PZ_10_1.py
+++ Proj_1sem_Bondarev/PZ_10/PZ_10_1.py
@@ -9,26 +9,31 @@
 
 import random
 f1 = open('new_file_1.txt', 'w', encoding='UTF-8')
-f1.write(str([random.randint(-10, 10) for i in range(10)]))
-f1.write(" ")
+for i in range(10):
+    f1.write(str(random.randint(-10, 10)))
+    f1.write(" ")
 f1.close()
 
 
 f2 = open('new_file_2.txt', 'w', encoding='UTF-8')
-f2.write(str([random.randint(-10, 10) for i in range(10)]))
-f2.write(" ")
+for i in range(10):
+    f2.write(str(random.randint(-10, 10)))
+    f2.write(" ")
 f2.close()
 
-f1 = open('new_file_1.txt')
+f1 = open('new_file_1.txt', 'r')
 i = f1.read()
 f1.close()
-f2 = open('new_file_2.txt')
+f2 = open('new_file_2.txt', 'r')
 q = f2.read()
 f2.close()
 
-f3 = open('new_file_3.txt', 'w', encoding='UTF-8')
-f3.write(" ")
-f3.write(f1 + f2)
-f3.close()
 
+f3 = open('new_file_3.txt', 'w', encoding='UTF-8')
+f3.write("Исходные данные: \n")
+f3.write(i)
+f3.write(q)
+f3.write('\n')
+f3.close()
+print(len(i + q))
 
