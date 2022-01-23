a9894bed7203f58294936c8d43e36a26363f77a5
Dmitry <92572893+xDetroiDx@users.noreply.github.com>
2022-01-23 22:01:36 +0300

    Update PZ_10_1.py

--- Proj_1sem_Bondarev/PZ_10/PZ_10_1.py
+++ Proj_1sem_Bondarev/PZ_10/PZ_10_1.py
@@ -11,9 +11,10 @@ import random
 d = 0
 a = []
 b = []
+
 f1 = open('new_file_1.txt', 'w', encoding='UTF-8')
 for i in range(10):
-    a.append(random.randint(-10, 10))
+    a.append(random.randint(-20, 20))
     f1.write(str(a[i]))
     f1.write(" ")
 f1.close()
@@ -21,10 +22,11 @@ f1.close()
 
 f2 = open('new_file_2.txt', 'w', encoding='UTF-8')
 for i in range(10):
-    b.append(random.randint(-10, 10))
+    b.append(random.randint(-20, 20))
     f2.write(str(b[i]))
     f2.write(" ")
 f2.close()
+
 
 f1 = open('new_file_1.txt', 'r')
 i = f1.read()
@@ -42,7 +44,12 @@ f3.write('\n')
 f3.write("Количество элементов: \n")
 f3.write(str(len(a) + len(b)))
 f3.write('\n')
-
+f3.write("Сумма элементов: \n")
+f3.write(str(sum(a) + sum(b)))
+f3.write('\n')
+f3.write("Элементы, умноженные на минимальный элемент: \n")
+k = a + b
+d = [i * k[k.index(min(k))] for i in k]
+f3.write(str(d))
+f3.write('\n')
 f3.close()
-d += int([i])
-print(d)
